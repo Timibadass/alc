79fbe01ab45e5903b559ed76ae49b42966249fab
= <=>
2017-12-07 11:11:06 +0100

    corrected extension error

--- controllers/ResourceController.js
+++ controllers/ResourceController.js
@@ -68,6 +68,6 @@ exports.viewResource = async (req, res) => {
 
 exports.downloadResource = async (req, res) => {
     const resource = await Resource.findOne({slug: req.params.slug})
-    const pdf = `http://res.cloudinary.com/student-to-herokuapp-com/image/upload/v1/${resource.resource}`;
+    const pdf = `http://res.cloudinary.com/student-to-herokuapp-com/image/upload/v1/${resource.resource}pdf`;
     res.download(pdf, `${resource.title}.pdf`);
 };
